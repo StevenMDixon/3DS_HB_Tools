69a4095674a31236d680915678aca045a61fd011
steven <stevendixon1128@gmail.com>
2023-08-06 11:14:43 -0400

    Updated set path for windows citra

--- Windows/CitraSetPath.bat
+++ Windows/CitraSetPath.bat
@@ -1,3 +1,1 @@
-set user=%USERNAME%
-echo C:/Users/%user%/AppData/Roaming/Citra/%user%
-setx Citra "C:/Users/%user%/AppData/Roaming/Citra/"
+setx Citra "C:/Users/%USERNAME%/AppData/Local/Citra/nightly-mingw/citra-qt.exe"
